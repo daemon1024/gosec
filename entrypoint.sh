36f71131e1e84c7ccf24b125d37202cdc33df2d4
daemon1024 <barun1024@gmail.com>
2021-07-29 20:15:01 +0000

    accept working directory input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Args:
+# $1 - inputs.working-directory
+# $2 - inputs.args
+
+cd $1
+
 # Expand the arguments into an array of strings. This is requires because the GitHub action
 # provides all arguments concatenated as a single string.
-ARGS=("$@")
+ARGS=("$2")
 
-/bin/gosec ${ARGS[*]}
+# /bin/gosec ${ARGS[*]}
